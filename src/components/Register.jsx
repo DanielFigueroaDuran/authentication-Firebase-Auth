255d10660f772be4896ff4bef435345c9e651357
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-01-03 22:15:20 +0100

    Creando estilos a Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,31 +1,51 @@
-import {FaLock} from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 const Register = () => {
     return (
         <div className="flex items-center justify-center min-h-full  h-screen">
             <div className="flex flex-col justify-center items-center bg-[#fff] p-8 w-96  rounded-lg  shadow-lg shadow-black">
-                <h2>Register</h2>
-                <div className="icon ">
-                    <FaLock/>
+                <h2 className="text-[#2196f3] mb-5">Register</h2>
+                <div className="flex items-center justify-center text-2xl text-white p-4 w-[20%] rounded-[50%] mb-5 bg-[#2196f3] ">
+                    <FaLock />
                 </div>
-                <div className="form">
-                    <div className="box">
-                        <input type="text" placeholder="full Name" />
+                <div className="flex flex-col mt-5">
+                    <div className="mb-5">
+                        <input
+                            className="w-full outline-none border border-[#bdbdbd] p-[12px]"
+                            type="text"
+                            placeholder="full Name"
+                        />
                     </div>
-                    <div className="box">
-                        <input type="email" placeholder="E-mail" />
+                    <div className="mb-5">
+                        <input
+                            className="w-full outline-none border border-[#bdbdbd] p-[12px]"
+                            type="email"
+                            placeholder="E-mail" />
+
                     </div>
-                    <div className="box">
-                        <input type="text" placeholder="Country Name" />
+                    <div className="mb-5">
+                        <input
+                            className="w-full outline-none border border-[#bdbdbd] p-[12px]"
+                            type="text"
+                            placeholder="Country Nam
+                         e" />
                     </div>
-                    <div className="box">
-                        <input type="text" placeholder="Phone Number" />
+                    <div className="mb-5">
+                        <input
+                            className="w-full outline-none border border-[#bdbdbd] p-[12px]"
+                            type="text"
+                            placeholder="Phone Numbe
+                         r" />
                     </div>
-                    <div className="box">
-                        <input type="text" placeholder="Password" />
+                    <div className="mb-5">
+                        <input
+                            className="w-full outline-none border border-[#bdbdbd] p-[12px]"
+                            type="text"
+                            placeholder="Password" /
+                        >
                     </div>
-                    <button>Register</button>
-                </div>            
+                    <button className="bg-[#ffd54f] ">Register</button>
+                </div>
             </div>
         </div>
     )
