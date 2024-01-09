2ee6f54e9f1c1a312dc02a36042797e49e1b61ba
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-01-09 21:41:14 +0100

    refactorizandoel boton de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,18 @@
-
+import { useState } from "react";
 import LoginPhoto from "../assets/img/Login.jpg";
 import Retrato from "../assets/img/Retrato.jpg";
+import appFiewbase from "../firebase/firebase.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
+const auth = getAuth(appFiewbase);
 const Login = () => {
+    const [register, setRegister] = useState(false);
 
     return (
         <div className="container mx-auto mt-4 flex items-center justify-center p-10 ">
             <div className="">
                 <div className="flex items-center h-full">
-                    <div className="border border-black p-4 rounded-lg">
+                    <div className="border border-black p-4 rounded-lg shadow-md shadow-black">
                         <img src={Retrato} alt="" className="w-[120px] h-[120px] m-auto rounded-[50%] mt-5 mb-5 " />
                         <form>
                             <input
@@ -23,9 +27,12 @@ const Login = () => {
                                 type="text"
                                 placeholder="Ingresar Contraseña"
                             />
-                            <button className="bg-[#0D6EFD] text-white border border-[slategray] rounded-[30px] p-4 mb-5 w-full">Registrarse</button>
+                            <button className="bg-[#0D6EFD] text-white border border-[slategray] rounded-[30px] p-4 mb-5 w-full">{register ? "Registrarse" : "Inicia Sesion"}</button>
                         </form>
-                        <h4><button>iniciar sesion</button></h4>
+                        <h4>
+                            {register ? "Si ya tienes cuenta " : " No tienes cuenta "}
+                            <button onClick={()=>{setRegister(!register)}}> {register ? " Iniciar sesion" : "Registrate" }</button>
+                        </h4>
                     </div>
                 </div>
             </div>
